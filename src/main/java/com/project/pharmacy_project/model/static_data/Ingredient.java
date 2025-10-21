7b588f8ad983172a1fb76da5c5578c62d8198a0e
liakosV <billiak@hotmail.com>
2025-10-21 18:40:41 +0300

    feat: New ingredient entity created

---
+++ src/main/java/com/project/pharmacy_project/model/static_data/Ingredient.java
@@ -0,0 +1,29 @@
+package com.project.pharmacy_project.model.static_data;
+
+import com.project.pharmacy_project.model.Drug;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ingredients")
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "ingredients")
+    private Set<Drug> drugs = new HashSet<>();
+}
