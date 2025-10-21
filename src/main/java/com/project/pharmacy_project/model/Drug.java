65216f3822b4e5891e107668af7c235c12432c1a
liakosV <billiak@hotmail.com>
2025-10-21 18:34:34 +0300

    feat: toOrder, overTheCounter and ingredient variables added in the drug entity

--- src/main/java/com/project/pharmacy_project/model/Drug.java
+++ src/main/java/com/project/pharmacy_project/model/Drug.java
@@ -1,6 +1,7 @@
 package com.project.pharmacy_project.model;
 
 import com.project.pharmacy_project.model.static_data.Category;
+import com.project.pharmacy_project.model.static_data.Ingredient;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
@@ -10,6 +11,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -34,9 +39,25 @@ public class Drug {
     @Min(0)
     private int quantity;
 
+    @Column(name = "to_order", nullable = false)
+    @Min(0)
+    private int toOrder;
+
+    @Column(name = "over_the_counter", nullable = false)
+    private boolean overTheCounter;
+
+    @ManyToMany
+    @JoinTable(
+            name = "drug_ingredients",
+            joinColumns = @JoinColumn(name = "drug_id"),
+            inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
+    private Set<Ingredient> ingredients = new HashSet<>();
+
     @ManyToOne(optional = false)
     @JoinColumn(name = "category_id")
     private Category category;
 
+    @OneToMany(mappedBy = "drug", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<StockMovement> stockMovements = new ArrayList<>();
 
 }
